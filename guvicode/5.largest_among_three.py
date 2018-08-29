8a0caeb5b49727cc0d79fe8839008b8aab296114
yogeshdeveloper <spyogesh1994@gmail.com>
2018-08-29 15:14:29 +0530

    Largest among three
    Commiting

---
+++ guvicode/5.largest_among_three.py
@@ -0,0 +1,10 @@
+a=int(input('Enter Number 1: '))
+b=int(input('Enter Number 2: '))
+c=int(input('Enter Number 3: '))
+
+if(a>b and a>c):
+    print(a)
+elif (b>a and b>c):
+    print(b)
+else:
+    print(c)   
