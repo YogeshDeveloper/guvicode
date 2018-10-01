cf256cb6ce4bc7a7b07e63d7c9a64bee1b70cf7c
Yogesh <spyogesh1994@gmail.com>
2018-10-01 11:15:28 +0530

    oddNumberBetweenIntervals

---
+++ guvicode/14.oddNumberBetweenIntervals.py
@@ -0,0 +1,11 @@
+a = int(input("Enter the starting interval: "))
+b = int(input("Enter the ending interval: "))
+k=[]
+for i in range(a,b):
+    #print(i)
+    if(i%2!=0):
+       # print(i)
+        k.append(i)
+print(k)        
+#for i in k:
+#    print(i)
